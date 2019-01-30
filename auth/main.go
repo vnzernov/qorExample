f6524b2ac3d81d730a8f59fff4ed00838ab7381b
zvn <zvn@gascom.ru>
2019-01-30 14:46:49 +0300

    2

---
+++ auth/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/qor/admin"
+	"github.com/qor/auth"
+	"github.com/qor/auth/auth_identity"
+	"github.com/qor/auth_themes/clean"
+	"github.com/qor/qor"
+	"github.com/qor/roles"
+	"github.com/qor/session/manager"
+)
+
+// Define another GORM-backend model
+type Product struct {
+	gorm.Model
+	Name        string
+	Description string
+}
+
+var (
+	// Initialize gorm DB
+	gormDB, _ = gorm.Open("sqlite3", "sample.db")
+
+	Auth = clean.New(&auth.Config{
+		DB: gormDB,
+		//Mailer:    config.Mailer,
+		UserModel: User{},
+	})
+	// Authority initialize Authority for Authorization
+	//Authority = authority.New(&authority.Config{
+	//	Auth: Auth,
+	//})
+)
+
+type AdminAuth struct{}
+
+func (AdminAuth) LoginURL(c *admin.Context) string {
+	return "/auth/login"
+}
+
+func (AdminAuth) LogoutURL(c *admin.Context) string {
+	return "/auth/logout"
+}
+
+func (AdminAuth) GetCurrentUser(c *admin.Context) qor.CurrentUser {
+	currentUser := Auth.GetCurrentUser(c.Request)
+	if currentUser != nil {
+		qorCurrentUser, ok := currentUser.(qor.CurrentUser)
+		if !ok {
+			fmt.Printf("User %#v haven't implement qor.CurrentUser interface\n", currentUser)
+		}
+		return qorCurrentUser
+	}
+	return nil
+}
+
+func init() {
+	roles.Register("admin", func(req *http.Request, currentUser interface{}) bool {
+		return currentUser != nil && currentUser.(*User).Role == "Admin"
+	})
+}
+
+func main() {
+
+	gormDB.LogMode(true)
+	gormDB.AutoMigrate(&User{}, &Product{}, &auth_identity.AuthIdentity{})
+	Admin := admin.New(&admin.AdminConfig{DB: gormDB, Auth: &AdminAuth{}})
+
+	Admin.AddResource(&User{})
+	Admin.AddResource(&Product{})
+
+	// Initalize an HTTP request multiplexer
+	mux := http.NewServeMux()
+
+	// Mount admin to the mux
+	Admin.MountTo("/", mux)
+
+	// Mount Auth to Router
+	mux.Handle("/auth/", Auth.NewServeMux())
+
+	fmt.Println("Listening on: 9000")
+	http.ListenAndServe(":9000", manager.SessionManager.Middleware(mux))
+}
+
+type User struct {
+	gorm.Model
+	Email                  string `form:"email"`
+	Password               string
+	Name                   string `form:"name"`
+	Gender                 string
+	Role                   string
+	Birthday               *time.Time
+	Balance                float32
+	DefaultBillingAddress  uint `form:"default-billing-address"`
+	DefaultShippingAddress uint `form:"default-shipping-address"`
+	//	Addresses              []Address
+	//Avatar AvatarImageStorage
+
+	// Confirm
+	ConfirmToken string
+	Confirmed    bool
+
+	// Recover
+	RecoverToken       string
+	RecoverTokenExpiry *time.Time
+
+	// Accepts
+	AcceptPrivate bool `form:"accept-private"`
+	AcceptLicense bool `form:"accept-license"`
+	AcceptNews    bool `form:"accept-news"`
+}
+
+func (user User) DisplayName() string {
+	return user.Email
+}
---
+++ start/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/qor/admin"
+)
+
+// Define a GORM-backend model
+type User struct {
+	gorm.Model
+	Name string
+}
+
+// Define another GORM-backend model
+type Product struct {
+	gorm.Model
+	Name        string
+	Description string
+}
+
+func main() {
+	// Set up the database
+	DB, _ := gorm.Open("sqlite3", "demo.db")
+	DB.AutoMigrate(&User{}, &Product{})
+
+	// Initalize
+	Admin := admin.New(&admin.AdminConfig{DB: DB})
+
+	// Create resources from GORM-backend model
+	Admin.AddResource(&User{})
+	Admin.AddResource(&Product{})
+
+	// Initalize an HTTP request multiplexer
+	mux := http.NewServeMux()
+
+	// Mount admin to the mux
+	Admin.MountTo("/admin", mux)
+
+	fmt.Println("Listening on: 9000")
+	http.ListenAndServe(":9000", mux)
+}
